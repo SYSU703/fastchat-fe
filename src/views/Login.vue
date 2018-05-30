d16ca0d8a3be0a61cfa2655b1789b8299a6aa247
Sirui Chen <632882184@qq.com>
2018-05-30 22:36:10 +0800

    初始化登录页和注册页的路由

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>login</h1>
+    <Slider v-model="value" range></Slider>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  data() {
+    return {
+      value: [20, 50],
+    };
+  },
+});
+</script>
