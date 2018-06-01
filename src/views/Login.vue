d509609225eb7c7915a2e949979d0df4c44bd5ee
Sirui Chen <632882184@qq.com>
2018-06-01 16:27:54 +0800

    完成基本登陆页面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,58 +1,72 @@
 <template>
-  <Form ref="formInline"
-        :model="formInline"
-        :rules="ruleInline"
-        inline>
-    <FormItem prop="user">
-      <Input type="text"
-             v-model="formInline.user"
-             placeholder="Username">
-      <Icon type="ios-person-outline"
-            slot="prepend" />
-      </Input>
-    </FormItem>
-    <FormItem prop="password">
-      <Input type="password"
-             v-model="formInline.password"
-             placeholder="Password">
-      <Icon type="ios-locked-outline"
-            slot="prepend" />
-      </Input>
-    </FormItem>
-    <FormItem>
-      <Button type="primary"
-              @click="handleSubmit('formInline')">Signin</Button>
-    </FormItem>
-  </Form>
+  <Card style="width: 480px; margin: 0 auto;">
+    <h1 slot="title">登陆</h1>
+    <Form id="login-form"
+          ref="loginForm"
+          :model="fields"
+          :rules="validateRules">
+
+      <FormItem prop="userName">
+        <Input type="text"
+               v-model="fields.userName"
+               placeholder="用户名">
+        <Icon type="ios-person-outline"
+              slot="prepend" />
+        </Input>
+      </FormItem>
+
+      <FormItem prop="password">
+        <Input type="password"
+               v-model="fields.password"
+               placeholder="密码">
+        <Icon type="ios-locked-outline"
+              slot="prepend" />
+        </Input>
+      </FormItem>
+
+      <FormItem>
+        <Button type="primary"
+                @click="handleSubmit()"
+                style="float: right;">登陆</Button>
+        <a style="float: right; margin-right: 24px;">注册</a>
+      </FormItem>
+
+    </Form>
+  </Card>
 </template>
+
+<style lang="stylus" scoped>
+</style>
+
+
 
 <script lang="ts">
 import Vue from 'vue';
 export default Vue.extend({
   data() {
     return {
-      formInline: {
-        user: '',
+      fields: {
+        userName: '',
         password: '',
       },
-      ruleInline: {
-        user: [
+      validateRules: {
+        userName: [
           {
             required: true,
-            message: 'Please fill in the user name',
+            message: '请输入用户名',
             trigger: 'blur',
           },
         ],
         password: [
           {
             required: true,
-            message: 'Please fill in the password.',
+            message: '请输入密码',
             trigger: 'blur',
           },
           {
             type: 'string',
             min: 6,
-            message: 'The password length cannot be less than 6 bits',
+            message: '密码不少于6个字符',
             trigger: 'blur',
           },
         ],
@@ -60,8 +74,8 @@ export default Vue.extend({
     };
   },
   methods: {
-    handleSubmit(name: string) {
-      (this.$refs[name] as any).validate((valid: boolean) => {
+    handleSubmit() {
+      (this.$refs.loginForm as any).validate((valid: boolean) => {
         if (valid) {
           this.$Message.success('Success!');
         } else {
